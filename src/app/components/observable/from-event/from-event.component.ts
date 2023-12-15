49128852894fb62f9a27ae66ee8cb9f07f36ae39
satish <satishkonduru0225@gmail.com>
2023-12-15 10:56:50 +0530

    fromEvent

--- src/app/components/observable/from-event/from-event.component.ts
+++ src/app/components/observable/from-event/from-event.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-from-event',
   templateUrl: './from-event.component.html',
   styleUrls: ['./from-event.component.css']
 })
-export class FromEventComponent {
+export class FromEventComponent implements AfterViewInit{
+
+@ViewChild('addUser') addUser : ElementRef
 constructor(private _router: Router){}
 back(){
   this._router.navigate(['/observable'])
 }
 
-fromEvent(){
-  
+ngAfterViewInit(): void {
+  let count = 1
+  fromEvent(this.addUser.nativeElement,'click')
+  .subscribe(res => {
+    let countValue = 'User - '+count++
+    console.log(countValue)
+    this.displayUser(countValue)
+  })
 }
+
+displayUser(val){
+  let element = document.createElement('li')
+  element.innerText = val
+  document.getElementById('users').appendChild(element)
+}
+
 }
